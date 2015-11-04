805ef4c1f954c1dc81b115ec9f0dc97741883a83
Sharon <shouyuan0326@gmail.com>
2015-11-04 15:26:43 -0500

    addProject

---
+++ app/src/main/java/com/example/sharon/myapplication/MainActivity.java
@@ -0,0 +1,196 @@
+package com.example.sharon.myapplication;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.util.Log;
+import android.widget.ImageView;
+
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginFragment;
+import com.facebook.AccessToken;
+import com.facebook.FacebookSdk.InitializeCallback;
+import com.facebook.Profile;
+import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    private   LoginButton loginButton;
+    private CallbackManager callbackManager=CallbackManager.Factory.create();
+    private static final String TAG="findit";
+    private String usrID,usrName;
+    private Bitmap bitmap;
+    ImageView user_image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday"));
+
+
+        Log.i(TAG, "onCreate");
+        user_image=(ImageView)findViewById(R.id.imageView);
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                // Application code
+                                usrID=response.getJSONObject().optString("id");
+                                Log.i(TAG, "---"+usrID);
+                                usrName=response.getJSONObject().optString("name");
+                                Log.i(TAG, "---"+usrName);
+                                Log.i(TAG, "---"+response.toString());
+
+                                bitmap=getFacebookProfilePicture(usrID);
+                                user_image.setImageBitmap(bitmap);
+                            }
+
+                        });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i(TAG, "onCancel");
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.i(TAG, "onError");
+                // App code
+            }
+
+
+        });
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Log.i(TAG, "OnClick");
+            }
+
+        });
+    }
+
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+
+
+
+            int SDK_INT = android.os.Build.VERSION.SDK_INT;
+            if (SDK_INT > 8)
+            {
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                        .permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                //your codes here
+
+            }
+        return super.onCreateView(parent, name, context, attrs);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.i(TAG, "onCreateooo");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        Log.i(TAG, "on " + id + "-------id");
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, android.content.Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        Log.i(TAG, "on " + "-----");
+
+    }
+    public static Bitmap getFacebookProfilePicture(String userID) {
+        URL imageURL = null;
+        Bitmap bitmap=null;
+        try {
+            imageURL = new URL("https://graph.facebook.com/" + userID + "/picture?type=large");
+            bitmap = BitmapFactory.decodeStream(imageURL.openConnection().getInputStream());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            Log.i(TAG, "err" + "malf");
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.i(TAG, "err" + "IO");
+        }
+        return bitmap;
+    }
+}
