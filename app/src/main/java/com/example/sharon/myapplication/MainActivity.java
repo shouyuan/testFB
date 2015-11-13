f372ade9c0ca2417a40068cbca3f4da7a8acc92c
DanielLee <jli57@stevens.edu>
2015-11-13 15:00:43 -0500

    welcome page
    Pleasant Goat and Big Big Wolf

--- app/src/main/java/com/example/sharon/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sharon/myapplication/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, personInfoActivity.class);
+                Intent i = new Intent(MainActivity.this, WelcomePageActivity.class);
                 startActivity(i);
 
             }
